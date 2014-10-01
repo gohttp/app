6076831a5fdccc44f3eb7c6640c10b585a7e7281
TJ Holowaychuk <tj@vision-media.ca>
2014-10-01 15:21:36 -0700

    add handler coercion. Closes #1
    kinda ugly, not sure how I feel about it but might as well go with it for a while

--- app.go
+++ app.go
@@ -23,9 +23,53 @@ func (a *App) Use(mw ...alice.Constructor) {
 	a.chain = a.chain.Append(mw...)
 }
 
+// Head will register a pattern with a handler for HEAD requests.
+func (a *App) Head(pat string, h interface{}) {
+	a.Add("HEAD", pat, handler(h))
+}
+
+// Get will register a pattern with a handler for GET requests.
+// It also registers pat for HEAD requests.
+func (a *App) Get(pat string, h interface{}) {
+	a.Add("HEAD", pat, handler(h))
+	a.Add("GET", pat, handler(h))
+}
+
+// Post will register a pattern with a handler for POST requests.
+func (a *App) Post(pat string, h interface{}) {
+	a.Add("POST", pat, handler(h))
+}
+
+// Put will register a pattern with a handler for PUT requests.
+func (a *App) Put(pat string, h interface{}) {
+	a.Add("PUT", pat, handler(h))
+}
+
+// Del will register a pattern with a handler for DELETE requests.
+func (a *App) Del(pat string, h interface{}) {
+	a.Add("DELETE", pat, handler(h))
+}
+
+// Options will register a pattern with a handler for OPTIONS requests.
+func (a *App) Options(pat string, h interface{}) {
+	a.Add("OPTIONS", pat, handler(h))
+}
+
 // Listen on `addr`.
 func (a *App) Listen(addr string) error {
 	handler := a.chain.Then(a)
 	http.Handle("/", handler)
 	return http.ListenAndServe(addr, nil)
+}
+
+// coerce handler into an http.Handler.
+func handler(h interface{}) http.Handler {
+	switch h.(type) {
+	case func(w http.ResponseWriter, r *http.Request):
+		return http.HandlerFunc(h.(func(w http.ResponseWriter, r *http.Request)))
+	case http.Handler:
+		return h.(http.Handler)
+	default:
+		panic("invalid handler")
+	}
 }
--- examples/main.go
+++ examples/main.go
@@ -7,9 +7,13 @@ import "fmt"
 func main() {
 	app := app.New()
 
-	app.Get("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "hello world")
+	app.Get("/foo", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "foo")
 	}))
+
+	app.Get("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "bar")
+	})
 
 	app.Listen(":3000")
 }
